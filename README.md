728dec80c88cf4b31b4c6b3ee48f88fa6c80256c
oddfar <oddfar@163.com>
2021-05-17 10:25:44 +0800

    commit message

---
+++ README.md
@@ -0,0 +1,111 @@
+使用[vuepress](https://vuepress.vuejs.org/zh)搭建，自动部署在[GitHub Pages](https://pages.github.com/) 
+
+使用[vdoing](https://github.com/xugaoyi/vuepress-theme-vdoing)主题
+
+## 准备
+
+VuePress 需要 [Node.js ](https://nodejs.org/en/)>= 8.6
+
+1. 克隆到本地并进入目录
+
+   ```sh
+   git clone https://github.com/oddfar/docs.git && cd docs
+   ```
+
+2. 安装本地依赖
+
+   ```sh
+   npm install
+   ```
+
+3. 本地测试
+
+   ```sh
+   npm run dev
+   ```
+
+   默认访问链接：http://localhost:8080/doc
+
+## 写作
+
+使用`markdown`语法编写`md`文件，所有笔记`md`文件放在`docs/docs`目录下
+
+例如添加`test`类，并编写`hello.md`文件
+
+1. 创建目录
+
+   格式：序号+标题
+
+   例如：30.test
+
+2. 添加笔记
+
+   例如：01.hello.md
+
+3. 编写内容
+
+   ```markdown
+   ---
+   title: 笔记标题
+   permalink: /test/hello/
+   date： 2021-01-01 01:01:01
+   ---
+   
+   ## 标题
+   
+   hello world
+   ```
+
+   tittle：标题，不填写则默认文件名中的标题，即`hello`
+
+   permalink：访问链接，不填写则自动生成
+
+   date：日期，默认文件创建时间
+
+4. 测试运行
+
+   在项目根目录下
+
+   ```sh
+   npm run dev
+   ```
+
+详情请看[vdoing主题介绍文档](https://doc.xugaoyi.com/)
+
+## 部署
+
+### 手动部署到github
+
+创建分支：`gh-pages`
+
+更改文件`deploy.sh`内容
+
+```sh
+githubUrl=git@github.com:oddfar/docs.git
+```
+
+
+
+```sh
+githubUrl=https://oddfar:${GITHUB_TOKEN}@github.com/oddfar/docs.git
+```
+
+双击运行`deploy.sh`
+
+### 自动部署到github
+
+待补充
+
+### 部署到自己服务器
+
+根目录下执行命令
+
+```sh
+npm run build
+```
+
+生成文件在`docs\.vuepress\dist\`目录下
+
+打包到服务器即可
+
+注：本地不可直接访问，需要配合插件
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,59 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: 'Java', link: '/java/', items: [
+      {
+        text: 'Java-Se', items: [
+          { text: '前言', link: '/java/se/preface/' },
+          { text: 'Java基础', link: '/java/se/initial-java/' },
+          { text: 'Java面向对象', link: '/java/se/object/' },
+          { text: 'Java常用类', link: '/java/se/commonly-used-class/' },
+          { text: 'Java集合框架', link: '/java/se/aggregate/synopsis/' },
+
+        ]
+      },
+	  {
+        text: 'Java-Se进阶', items: [
+          { text: '多线程', link: '/java/se/thread/study-note/' },
+
+        ]
+      },
+      {
+        text: 'Java-ee', items: [
+          { text: 'MySQL', link: '/mysql/overview/' },
+          { text: 'JavaWeb', link: '/javaweb/basic-concepts/' },
+        ]
+      },
+
+    ]
+  },
+  {
+    text: 'Web',
+    items: [
+      { text: 'HTML', link: '/html5/overview/' },
+      { text: 'CSS', link: '/css/overview/' },
+      { text: 'JavaScript', link: '/javascript/overview/' },
+      { text: 'vue', link: '/vue/overview/' },
+    ]
+  },
+  {
+    text: '框架',
+    items: [
+      { text: 'MyBatis', link: '/mybatis/study-note/' },
+      { text: 'MyBatis-Plus', link: '/mybatis-plus/study-note/' },
+	  { text: 'Spring', link: '/spring/study-note/' },
+      { text: 'Redis', link: '/redis/study-note/' },
+	  
+	  
+    ]
+  },
+  {
+    text: '其他',
+    items: [
+      { text: 'C语言', link: '/c/' },
+	  { text: 'git', link: '/git/' },
+    ]
+  },
+  { text: '关于', link: '/about/' },
+  { text: '归档', link: '/archives/' }
+]
