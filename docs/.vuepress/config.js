8f5c53fd13696c99dac80eed8e1d53b06ad2afcb
oddfar <oddfar@163.com>
2021-06-05 18:03:47 +0800

    :art: docker

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,20 @@
-const nav = require('./config/nav.js');
+const plugins = require('./config/plugins.js');
 const base = require('../../base.js');
-const htmlModules = require('./config/htmlModules.js');
+const themeConfig = require('./config/themeConfig.js');
+
+
 
 module.exports = {
+  theme: 'vdoing', // 使用依赖包主题
+  port: 8080,//端口
+
+  plugins,
+  themeConfig,
+  base,  //引入后缀
+
   title: "OddFar's Notes",
   description: '我们在黑暗中并肩前行，走在各自的朝圣路上!', // 描述,以 <meta> 标签渲染到页面html中
-  base,  //后缀 '/docs/'
+
   head: [
     ['link', { rel: 'icon', href: '/img/favicon.ico' }],
     ['meta', { name: 'keywords', content: 'oddfar,zhiyuan' }],
@@ -14,133 +23,4 @@ module.exports = {
   markdown: {
     lineNumbers: true // 代码行号
   },
-
-  port: 8080,
-
-  theme: 'vdoing', // 使用依赖包主题
-
-  themeConfig: { // 主题配置
-    nav,
-    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png', // 导航栏logo
-    repo: 'oddfar/notes', // 导航栏右侧生成Github链接
-    searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: '最后更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
-    docsDir: 'docs', // 编辑的文件夹
-    editLinks: true, // 启用编辑
-    editLinkText: '在 GitHub 上编辑此页',
-
-    // 以下配置是Vdoing主题改动的和新增的配置
-    category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
-    tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
-    // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
-    // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
-    // bodyBgImg: [
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
-    // ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
-    // titleBadge: false, // 文章标题前的图标是否显示，默认true
-    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
-    //   '图标地址1',
-    //   '图标地址2'
-    // ],
-
-
-
-    sidebar: 'structuring', // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
-
-    // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
-    updateBar: { // 最近更新栏
-      showToArticle: true, // 显示到文章页底部，默认true
-      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
-    },
-
-    author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
-      name: 'zhiyuan', // 必需
-      href: 'https://oddfar.com/' // 可选的
-    },
-    // blogger:{ // 博主信息，显示在首页侧边栏
-    //   avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-    //   name: 'Evan Xu',
-    //   slogan: '前端界的小学生'
-    // },
-    // social:{ // 社交图标，显示于博主信息栏和页脚栏
-    //   // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
-    //   icons: [
-    //     {
-    //       iconClass: 'icon-youjian',
-    //       title: '发邮件',
-    //       link: 'mailto:894072666@qq.com'
-    //     },
-    //     {
-    //       iconClass: 'icon-github',
-    //       title: 'GitHub',
-    //       link: 'https://github.com/xugaoyi'
-    //     },
-    //     {
-    //       iconClass: 'icon-erji',
-    //       title: '听音乐',
-    //       link: 'https://music.163.com/#/playlist?id=755597173'
-    //     }
-    //   ]
-    // },
-    footer: { // 页脚信息
-      createYear: 2021, // 博客创建年份
-      copyrightInfo: 'oddfar | notes', // 博客版权信息，支持a标签
-    }
-  },
-  plugins: [ // 插件
-    ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-      thirdparty: [ // 可选，默认 []
-        {
-          title: '在MDN中搜索',
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-          behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
-        },
-        {
-          title: '在Bing中搜索',
-          frontUrl: 'https://cn.bing.com/search?q='
-        }
-      ]
-    }],
-
-    // 'vuepress-plugin-baidu-autopush', // 百度自动推送
-
-    ['one-click-copy', { // 代码块复制按钮
-      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-      duration: 1000, // prompt message display time.
-      showInMobile: false // whether to display on the mobile side, default: false.
-    }],
-    ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-      settings: {
-        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
-        // cssLib: ['http://xxx'], // 在线示例中的css依赖
-        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
-        jsfiddle: false, // 是否显示 jsfiddle 链接
-        codepen: true, // 是否显示 codepen 链接
-        horizontal: false // 是否展示为横向样式
-      }
-    }],
-    [
-      'vuepress-plugin-zooming', // 放大图片
-      {
-        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
-        options: {
-          bgColor: 'rgba(0,0,0,0.6)'
-        },
-      },
-    ],
-
-    [
-      '@vuepress/last-updated', // "上次更新"时间格式
-      {
-        transformer: (timestamp, lang) => {
-          const moment = require('moment') // https://momentjs.com/
-          return moment(timestamp).format('YYYY/MM/DD, H:MM:SS');
-        }
-      }
-    ]
-  ]
 }
---
+++ docs/.vuepress/config/plugins.js
@@ -0,0 +1,64 @@
+//插件
+module.exports = [
+    //本地插件
+    [require('../plugins/love-me'), { // 鼠标点击爱心特效
+        color: '#11a8cd', // 爱心颜色，默认随机色
+        excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+    }],
+
+    ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+        thirdparty: [ // 可选，默认 []
+            {
+                title: '在MDN中搜索',
+                frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+                behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+            },
+            {
+                title: '在Bing中搜索',
+                frontUrl: 'https://cn.bing.com/search?q='
+            }
+        ]
+    }],
+
+
+
+    // 'vuepress-plugin-baidu-autopush', // 百度自动推送
+
+    ['one-click-copy', { // 代码块复制按钮
+        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        duration: 1000, // prompt message display time.
+        showInMobile: false // whether to display on the mobile side, default: false.
+    }],
+    ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+        settings: {
+            // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+            // cssLib: ['http://xxx'], // 在线示例中的css依赖
+            // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+            jsfiddle: false, // 是否显示 jsfiddle 链接
+            codepen: true, // 是否显示 codepen 链接
+            horizontal: false // 是否展示为横向样式
+        }
+    }],
+    [
+        'vuepress-plugin-zooming', // 放大图片
+        {
+            selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+            options: {
+                bgColor: 'rgba(0,0,0,0.6)'
+            },
+        },
+    ],
+
+    [
+        '@vuepress/last-updated', // "上次更新"时间格式
+        {
+            transformer: (timestamp, lang) => {
+                const moment = require('moment') // https://momentjs.com/
+                return moment(timestamp).format('YYYY/MM/DD, H:MM:SS');
+            }
+        }
+    ]
+
+
+]
